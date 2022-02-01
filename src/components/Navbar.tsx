73067a940c2bd851b97a023077e01149bad8a88e
Sardor <sardor.jurayev@elev.ntig.se>
2022-02-01 13:34:11 +0100

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 
   const Navbar=()=>{
     return(
-      <div className="bg-purple-900 w-20 h-[96vh] top-5 left-5 fixed">
+      <div className="bg-purple-900 w-36 h-[96vh] top-5 left-5 fixed">
         <h1 className="text-zinc-50">Admin</h1>
         <hr />
         <div className=""></div>
