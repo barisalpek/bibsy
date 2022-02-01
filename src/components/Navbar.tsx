c02654e953edc1436887f7976f0a3720b24f4788
Sardor <sardor.jurayev@elev.ntig.se>
2022-02-01 15:43:30 +0100

    main

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,16 +1,53 @@
 import React from 'react';
 
-
-  const Navbar=()=>{
+  const Navbar = () =>{
     return(
-      <div className="bg-purple-900 w-36 h-[96vh] top-5 left-5 fixed">
-        <h1 className="text-zinc-50">Admin</h1>
-        <hr />
-        <div className=""></div>
-        <div className=""></div>
-        <div className=""></div>
-        <div className=""></div>
-        <div className=""></div>
+      <div className="bg-purple-900 w-72 h-[96vh] top-5 left-5 fixed rounded-l-2xl">
+        <div>
+          <img src="" alt="admin" />
+        <h1 className="text-zinc-50 text-center pt-10 pb-10 w-100">Admin</h1>
+        </div>
+        <hr className="pt-5" />
+
+        <a href="" className="w-[100%]">
+        <div className="text-center pt-5 pb-5 text-zinc-50 hover:bg-purple-800">
+            <p className="w-[100%]">
+            Dashboard
+            </p>
+        </div>
+        </a>
+
+        <a href="" className="w-[100%]">
+        <div className="text-center pt-5 pb-5 text-zinc-50 hover:bg-purple-800">
+            <p className="w-[100%]">
+            Members
+            </p>
+        </div>
+        </a>
+
+        <a href="" className="w-[100%]">
+        <div className="text-center pt-5 pb-5 text-zinc-50 hover:bg-purple-800">
+            <p className="w-[100%]">
+            Books
+            </p>
+        </div>
+        </a>
+
+        <a href="" className="w-[100%]">
+        <div className="text-center pt-5 pb-5 text-zinc-50 hover:bg-purple-800">
+            <p className="w-[100%]">
+            Returned
+            </p>
+        </div>
+        </a>
+
+        <a href="" className="w-[100%]">
+        <div className="text-center pt-5 pb-5 text-zinc-50 hover:bg-purple-800">
+            <p className="w-[100%]">
+            Not Returned
+            </p>
+        </div>
+        </a>
       </div>
     )
 
