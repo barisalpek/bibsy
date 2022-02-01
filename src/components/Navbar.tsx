d50e15cd7a625ca9d045e90ec39beb187d285787
Sardor <sardor.jurayev@elev.ntig.se>
2022-02-01 13:13:43 +0100

    sa

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,17 +1,19 @@
-import React from "react";
+import React from 'react';
 
 
-  const Navbar = () =>{
-    return() => {
-      <div className={'navbar'}>
-        <h1>Admin</h1>
+  const Navbar=()=>{
+    return(
+      <div className="bg-purple-900 w-20 h-[96vh] top-5 left-5 fixed">
+        <h1 className="text-zinc-50">Admin</h1>
         <hr />
-        <div className={'navbar-dashboard'}></div>
-        <div className={'navbar-members'}></div>
-        <div className={''}></div>
-        <div className={'navbar-dashboard'}></div>
-        <div className={'navbar-dashboard'}></div>
+        <div className=""></div>
+        <div className=""></div>
+        <div className=""></div>
+        <div className=""></div>
+        <div className=""></div>
       </div>
-    }
+    )
 
   }
+
+  export default Navbar;
