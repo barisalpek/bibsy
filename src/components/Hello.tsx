0f595e96bfd48df31c1ca0cb8490c74428438c94
j4w4d-0x4e <59806530+j4w4d-0x4e@users.noreply.github.com>
2022-02-01 08:59:21 +0100

    Initial commit

---
+++ src/components/Hello.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Hello=()=> {
+    return (
+        <div className="bg-black grid place-items-center h-screen">
+            <h1 className="text-lg text-violet-400 font-bold underline">
+                Hello There
+            </h1>
+        </div>
+       
+    );
+}
+export default Hello;
