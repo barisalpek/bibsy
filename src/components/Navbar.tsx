230b0694395d5e734aa3634d57de200210334e9c
Sardor <sardor.jurayev@elev.ntig.se>
2022-02-01 10:36:51 +0100

    hej

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+
+  const Navbar = () =>{
+    return() => {
+      <div className={'navbar'}>
+        <h1>Admin</h1>
+        <hr />
+        <div className={'navbar-dashboard'}></div>
+        <div className={'navbar-members'}></div>
+        <div className={''}></div>
+        <div className={'navbar-dashboard'}></div>
+        <div className={'navbar-dashboard'}></div>
+      </div>
+    }
+
+  }
