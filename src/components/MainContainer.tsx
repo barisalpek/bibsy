cf119d5d11a6d2341a833b2ef3fe62439a6df5f5
William-Wolke <60339769+William-Wolke@users.noreply.github.com>
2022-02-09 09:14:04 +0100

    React-router-dom, less passed props, return or borrow page
    Co-Authored-By: FilipHallgren <90610802+FilipHallgren@users.noreply.github.com>
    Co-Authored-By: NoahFerm <90610537+NoahFerm@users.noreply.github.com>
    Co-Authored-By: Leynuz <91313418+Leynuz@users.noreply.github.com>

--- src/components/MainContainer.tsx
+++ src/components/MainContainer.tsx
@@ -2,76 +2,115 @@ import React, {useEffect, useState} from 'react';
 import List from './List';
 import Navbar from './Navbar';
 import getData from './getData';
+import ReturnAndBorrow from './ReturnAndBorrow';
 //import Buttons from './Buttons';
+import {
+    BrowserRouter as Router,
+    Route,
+    Routes,
+  } from "react-router-dom";
 
-type typeOfPage = {
-    page: string;
+type Prop = {
     isAdmin: boolean;
 }
 
-const MainContainer = (props: typeOfPage) => {
+const MainContainer = (props: Prop) => {
 
-    const [pageType, setPageType] = useState<string>('');
+    
     const [navStyle, setNavStyle] = useState<string>('');
     const [bodyStyle, setBodyStyle] = useState<string>('');
     const [isMain, setIsMain] = useState<boolean | null>(null);
     const [isAdmin, setIsAdmin] = useState<boolean | null>(null);
 
+    
+
     useEffect(() => {
-        setPageType(props.page);
         setIsAdmin(props.isAdmin);
-
-
-        if (pageType === 'list') {
-            setNavStyle('');
-            setBodyStyle('');
-            setIsMain(false);
-        }
-        else if(pageType === 'main') {
-            setNavStyle('');
-            setBodyStyle('');
-            setIsMain(true);
-        }
-        else {
-            setIsMain(null); 
-        }
     }, [])
 
  return(
-     <div className="h-[90%] w-5/6 my-auto -z-[0] ">
-        {isMain && <>
-        {isAdmin && <Navbar/>}
-            <div className='h-[100%] w-[100%] bg-white rounded'>
-                {/*  */}
-                <div>
-                    {!isAdmin && <div>
-                    
-                    </div>}
-                </div>
-                <div>
-                   {/* <MainNavbar */} 
-                </div>     
-            </div>
-        </>}
-        {!isMain && 
-        <>
-            
-            <div className='h-[100%] w-[100%] flex  flex-row'>
-                {isAdmin && <Navbar/>}
-                <div className='w-full h-[100%] flex border-red-500 border-2 flex-col content-between'>
-                    <div className='h-[10%] mb-[3%] bg-blue-600 '>
-                        {/*<Buttons />*/}
+    <Router>
+        <div className="h-[90%] w-5/6 my-auto -z-[0] flex flex-row">
+        
+    {isAdmin && <Navbar/>}
+
+        <Routes>
+
+            <Route path="/" element={(
+                <>
+                <div className='h-[100%] w-[100%] bg-white rounded-2xl'>
+                        {/*  */}
+                            <div>
+                        {!isAdmin && <div></div>}
                     </div>
-                    
-                    <div className='h-[85%] w-full'>
-                        <List mainColor='grön' listColor='ljusGrön' typeOf='book' bookType="available"/>
-                    </div>    
-                </div> 
-            </div>
-            </>
-        }
-    </div>
+                    <div>
+                    {/* <MainNavbar */} 
+                    </div>     
+                </div>
+                </>
+            )}/> 
+
+            <Route path="/loanOutBaby" element={(
+
+                <ReturnAndBorrow/>
+
+            )}/>
+
+            <Route path="/memberList" element={(
+                <>
+                    <div className='h-[100%] w-[100%] flex  flex-row'>
+                        <div className='w-full h-[100%] flex border-red-500 border-2 flex-col content-between'>
+                            <div className='h-[10%] mb-[3%] bg-blue-600 '>
+                                {/*<Buttons />*/}
+                            </div>
+                        
+                            <div className='h-[85%] w-full'>
+                                <List mainColor='lila' listColor='ljusLila' typeOf='member' bookType="available"/>
+                            </div>
+                        </div> 
+                    </div>
+                </>
+            )}/> 
+
+            <Route path="/bookList" element={(
+                <>
+                <div className='h-[100%] w-[100%] flex  flex-row'>
+                        <div className='w-full h-[100%] flex border-red-500 border-2 flex-col content-between'>
+                            <div className='h-[10%] mb-[3%] bg-blue-600 '>
+                                {/*<Buttons />*/}
+                            </div>
+                        
+                            <div className='h-[85%] w-full'>
+                                <></>
+                                <List mainColor='grön' listColor='ljusGrön' typeOf='book' bookType="available"/>
+                            </div>
+                        </div> 
+                    </div>
+                </>
+            )}/>
+
+            <Route path="/loanedList" element={(
+                <>
+                <div className='h-[100%] w-[100%] flex  flex-row'>
+                        <div className='w-full h-[100%] flex border-red-500 border-2 flex-col content-between'>
+                            <div className='h-[10%] mb-[3%] bg-blue-600 '>
+                                {/*<Buttons />*/}
+                            </div>
+                        
+                            <div className='h-[85%] w-full'>
+                                <></>
+                                <List mainColor='röd' listColor='ljusRöd' typeOf='missing' bookType="borrowed"/>
+                            </div>
+                        </div> 
+                    </div>
+                </>
+            )}/>
+            
+        </Routes>
+        
+        </div>
+    </Router>
  );
 }
 
-export default MainContainer
+export default MainContainer;
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,12 +5,13 @@ import {SiBookstack} from 'react-icons/si'
 import {RiErrorWarningLine} from 'react-icons/ri';
 import {RiDashboardLine} from 'react-icons/ri';
 import {ImBooks} from 'react-icons/im';
+import { Link, NavLink } from 'react-router-dom';
 
-  const Navbar = () =>{
+  const Navbar = (props:any) =>{
     return(
       
       // Here is the main component of the navbar
-      <div className="bg-adminSidebar  w-[30vw] xl:w-[16vw] xl:p-[2vw] h-[100%] mr-[5%] items-center grid justify-center rounded-2xl overflow-hidden">
+      <div className="bg-adminSidebar  w-[35vw] xl:w-[16vw] xl:p-[2vw] h-[100%] mr-[5%] items-center grid justify-center rounded-2xl overflow-hidden">
         {/* Here is the icon, positioning (of) and admin */}
         <div className="grid place-items-center mb-3 pb-2">
           <label className="pt-9"><BiUserCircle color='white'size={'3rem'}/></label>
@@ -20,7 +21,7 @@ import {ImBooks} from 'react-icons/im';
         <hr className= "w-[80%] mx-auto" />
         {/* Here is the icon, positioning (of) and dashboard */}
         <div className="place-items-center w-[19vw]">  
-            <a href="" className="w-[100%]">
+            <NavLink to="/" className="w-[100%]">
               <div className="sidebar-options w-[full]">
                 <label className=""><RiDashboardLine color='white'size={'3rem'}/>
                 </label>
@@ -28,43 +29,41 @@ import {ImBooks} from 'react-icons/im';
                   Dashboard
                 </p>
               </div>
-            </a>
+            </NavLink>
         {/* Here is the icon, positioning (of) and members */}
-          <div>
+          
             {/* Will be attempting to add a dropdown */}
-            <button className="w-[100%]">
+            <NavLink to="/memberList" className="w-[100%]">
               <div className="sidebar-options">
                 <label className=""><FiUsers color='white'size={'3rem'}/></label>
                     <p className="sidebar-text">
                       Members
                     </p>
               </div> 
-           </button>
-          </div>
+           </NavLink>
             
         {/* Here is the icon, positioning (of) and books */}  
-            <a href="" className="w-[100%]">
+            <NavLink to="/bookList" className="w-[100%]">
               <div className="sidebar-options">
                 <label className=""><ImBooks color='white'size={'3rem'}/></label>
                     <p className="sidebar-text">
                       Books
                     </p>
               </div>
-            </a>
-
+            </NavLink>
             
         {/* Here is the icon, positioning (of) and NOT returned */}
-            <a href="" className="w-[100%]">
+            <NavLink to="/loanedList" className="w-[100%]">
                <div className="sidebar-options">
                  <label className=""><RiErrorWarningLine color='white'size={'3rem'}/></label>
                     <p className="sidebar-text">
                       Not Returned
                    </p>
              </div>
-            </a>
+            </NavLink>
         </div>
       </div>
-      
+
     )
   }
   export default Navbar;
